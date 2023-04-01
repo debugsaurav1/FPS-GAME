e970660cd76523b7c9d543b168f1e50ac8f518e4
debugsaurav1 <me.saurav2049@gmail.com>
2023-04-02 02:01:59 +1030

    added gun flash

---
+++ Assets/Scripts/BasicGun.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+
+
+
+public class BasicGun : MonoBehaviour
+{
+    [Header("Gun Properties")]
+    [SerializeField] public float damage;
+    [SerializeField] public float range;
+    [SerializeField] private KeyCode fireGun = KeyCode.Mouse0;
+
+    [SerializeField] public Camera fpsCameraRef;
+
+    private void Awake()
+    {
+        damage = 10;
+        range = 100;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(fireGun))
+        {
+            Shoot();
+        }
+    }
+    void Shoot() 
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCameraRef.transform.position, fpsCameraRef.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            EnemyMovement target = hit.transform.GetComponent<EnemyMovement>();
+
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
